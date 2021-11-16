0bf509aae620e0f341ed8e80b6dcf5c94fdba80b
FaisalTamanoJr <faisalrichardjr@gmail.com>
2021-11-16 23:18:22 +0300

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Vocabcom
+
+Vocabcom is scrapes Vocabulary.com for word definitions.
+
+## Dependencies
+
+Python 3.9.7
+
+## Installation
+
+1. Clone the repository
+```bash
+git clone https://github.com/faisaltamanojr/vocabcom.git
+```
+
+2. Go to the repository folder
+```bash
+cd vocabcom
+```
+
+3. Install it using pip
+```python
+pip install .
+```
+
+## Usage
+
+```bash
+vocabcom <command> [argument]
+```
+
+```
+The commands are:
+
+define		searches for the short definition of that word
+long-define	searches for the long definition of that word
+```
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+Licensed under the [MIT License](LICENSE).
---
+++ vocabcom/main.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import sys
+import requests
+
+def main():
+    # The commands in the program
+    commands = {
+            "define":"searches for the short definition of that word",
+            "long-define":"searches for the long definition of that word",
+            }
+
+    # Create a list for their keys and their values
+    command_keys = []
+    command_values = []
+    for command in commands.items():
+        command_keys.append(command[0])
+        command_values.append(command[1])
+
+
+    # Let the user provide one word
+    if len(sys.argv) == 2 or len(sys.argv) == 3:
+
+        if sys.argv[1] == "define":
+            Definition("short")
+
+        elif sys.argv[1] == "long-define":
+            Definition("long")
+
+        else:
+            print("Usage: vocabcom <command> [arguments] \n\nThe commands are:\n")
+            for i in range(len(commands)):
+                print("\t" + command_keys[i] + "\t\t" + command_values[i])
+
+    # If the user does not provide a word or provides too much words tell them how to properly use the program
+    else:
+        print("Usage: vocabcom <command> [arguments] \n\nThe commands are:\n")
+        for i in range(len(commands)):
+            print("\t" + command_keys[i] + "\t\t" + command_values[i])
+
+def Definition(length):
+    if len(sys.argv) == 3:
+        # The dictionary link to use
+        dictionary = "https://www.vocabulary.com/dictionary/"
+        url = dictionary + sys.argv[2]
+
+        # Send an http request to get the html file from the url
+        url = requests.get(url).text
+
+        # Parse it
+        soup = BeautifulSoup(url, "html.parser")
+
+        if length == "short":
+            # Look for the short definition and print it
+            short_definition = soup.find_all("p", class_="short")
+
+            # If it exists
+            if short_definition:
+                for short in short_definition:
+                    print(short.text)
+
+        elif length == "long":
+            # Look for the short definition and print it
+            long_definition = soup.find_all("p", class_="long")
+
+            # If it exists
+            if long_definition:
+                for long in long_definition:
+                    print(long.text)
+
+        # If the word or the short definition cannot be found
+        else:
+            print(f'The {length} definition of the word cannot be found')
+
+    else:
+        print("Usage: vobacom define <word>")
+
+if __name__ == "__main__":
+    exit(main())
